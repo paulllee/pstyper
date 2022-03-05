6e25511018296dfb516e378118b42a2a1f394c23
paulllee <paulllee.md@gmail.com>
2022-03-04 23:49:57 -0500

    added axios get to retrieve quotable api quotes

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+let axios = require("axios");
 const express = require("express");
 const app = express();
 
@@ -6,6 +7,15 @@ const hostname = "localhost";
 
 app.use(express.static("public"));
 app.use(express.json());
+
+const QUOTABLE_API_URL = 'http://api.quotable.io/random';
+
+app.get("/", function(req, res) {
+    axios.get(QUOTABLE_API_URL).then(function (response) {
+        res.status(200);
+        res.json({"quote": response.data});
+    });
+})
 
 app.listen(port, hostname, () => {
     console.log(`Listening at: http://${hostname}:${port}`);

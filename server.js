eaee3e956a0fe06f9c1a0073ba0fe05a75f5a177
paulllee <paulllee.md@gmail.com>
2022-03-05 17:43:23 -0500

    changes to index with timer and api changes

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const hostname = "localhost";
 app.use(express.static("public"));
 app.use(express.json());
 
-const QUOTABLE_API_URL = 'https://api.quotable.io/random?minLength=100';
+const QUOTABLE_API_URL = 'https://api.quotable.io/random?minLength=125';
 
 app.get("/quotable", function(req, res) {
     axios.get(QUOTABLE_API_URL).then(function (response) {
